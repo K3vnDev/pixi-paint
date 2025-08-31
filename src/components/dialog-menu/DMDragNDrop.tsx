55f4e995b6986e46eab786046751235924f63753
K3vnDev <kevinrpolo16@gmail.com>
2025-08-31 01:03:23 -0500

    Delete forwardRef from DMDragNDrop component

--- src/components/dialog-menu/DMDragNDrop.tsx
+++ src/components/dialog-menu/DMDragNDrop.tsx
@@ -1,6 +1,6 @@
 import { ColoredPixelatedImage } from '@@/ColoredPixelatedImage'
 import type { IconName, ReusableComponent } from '@types'
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { useTimeout } from '@/hooks/useTimeout'
 
@@ -10,124 +10,108 @@ type Props = {
   allowMultipleFiles?: boolean
   acceptedFormats: string[]
   onDropOrSelect: (contents: string[]) => void
-} & Omit<ReusableComponent, 'ref'>
+} & ReusableComponent
 
-export const DMDragNDrop = React.forwardRef<HTMLDivElement, Props>(
-  (
-    {
-      children = 'Drag & Drop here or click to choose',
-      icon = 'upload',
-      className = '',
-      allowMultipleFiles = false,
-      acceptedFormats,
-      onDropOrSelect,
-      style
-    },
-    refFromParent
-  ) => {
-    const elementRef = useRef<HTMLDivElement>(null)
-    const [isDragginOver, setIsDraggingOver] = useState(false)
-    const { startTimeout, stopTimeout } = useTimeout()
+export const DMDragNDrop = ({
+  children = 'Drag & Drop here or click to choose',
+  icon = 'upload',
+  className = '',
+  allowMultipleFiles = false,
+  acceptedFormats,
+  onDropOrSelect,
+  ...props
+}: Props) => {
+  const [isDragginOver, setIsDraggingOver] = useState(false)
+  const { startTimeout, stopTimeout } = useTimeout()
+  const DRAG_OVER_MIN = 220
 
-    // Combine both parent and internal refs
-    // DELETE MAYBE
-    const combinedRef = (el: HTMLDivElement | null) => {
-      elementRef.current = el
-      if (!refFromParent) return
+  useEffect(() => {
+    const handleDrop = (e: DragEvent) => {
+      e.preventDefault()
 
-      if (typeof refFromParent === 'function') refFromParent(el)
-      else refFromParent.current = el
+      if (e.dataTransfer) {
+        const { files } = e.dataTransfer
+        handleFiles(files)
+      }
     }
 
-    useEffect(() => {
-      const handleDrop = (e: DragEvent) => {
-        e.preventDefault()
+    const handleDragOver = (e: DragEvent) => {
+      e.preventDefault()
+      setIsDraggingOver(true)
 
-        if (e.dataTransfer) {
-          const { files } = e.dataTransfer
-          handleFiles(files)
-        }
-      }
-
-      const handleDragOver = (e: DragEvent) => {
-        e.preventDefault()
-        setIsDraggingOver(true)
-
-        stopTimeout()
-        startTimeout(() => {
-          setIsDraggingOver(false)
-        }, 200)
-      }
-
-      document.addEventListener('drop', handleDrop)
-      document.addEventListener('dragover', handleDragOver)
-
-      return () => {
-        document.removeEventListener('drop', handleDrop)
-        document.removeEventListener('dragover', handleDragOver)
-      }
-    }, [])
-
-    const handleClick = () => {
-      const input = document.createElement('input')
-      input.type = 'file'
-      input.accept = acceptedFormats.join(', ')
-      input.multiple = allowMultipleFiles
-      input.style.display = 'none'
-      document.body.appendChild(input)
-
-      input.onchange = async e => {
-        const { files } = e.target as HTMLInputElement
-        files && (await handleFiles(files))
-        document.body.removeChild(input)
-      }
-
-      input.click()
+      stopTimeout()
+      startTimeout(() => {
+        setIsDraggingOver(false)
+      }, DRAG_OVER_MIN)
     }
 
-    const handleFiles = async (fileList: FileList) => {
-      const readFile = (file: File): Promise<string> => {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader()
-          reader.onload = () => resolve(reader.result as string)
-          reader.onerror = () => reject(reader.error)
-          reader.readAsText(file)
-        })
-      }
+    document.addEventListener('drop', handleDrop)
+    document.addEventListener('dragover', handleDragOver)
 
-      const files = Array.from(fileList)
-      if (!files.length) return
+    return () => {
+      document.removeEventListener('drop', handleDrop)
+      document.removeEventListener('dragover', handleDragOver)
+    }
+  }, [])
 
-      const contents = await Promise.all(files.map(file => readFile(file)))
-      onDropOrSelect(contents)
+  const handleClick = () => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = acceptedFormats.join(', ')
+    input.multiple = allowMultipleFiles
+    input.style.display = 'none'
+    document.body.appendChild(input)
+
+    input.onchange = async e => {
+      const { files } = e.target as HTMLInputElement
+      files && (await handleFiles(files))
+      document.body.removeChild(input)
     }
 
-    const [twBaseStyles, twIconStyles, text] = isDragginOver
-      ? ['border-theme-10 animate-pulse', 'animate-bounce', 'Drop your files here!']
-      : ['', '', children]
-
-    return (
-      <div
-        ref={combinedRef}
-        className={twMerge(`
-          relative border-4 border-theme-10/20 border-dashed rounded-xl
-          flex flex-col gap-2 items-center justify-center p-8
-          hover:border-theme-10/40 active:scale-99 active:brightness-90 transition
-          ${twBaseStyles} ${className}
-      `)}
-        onClick={handleClick}
-        style={style}
-      >
-        <ColoredPixelatedImage icon={icon} className={`size-16 ${twIconStyles}`} />
-        <span
-          className={`
-            h-16 flex items-center justify-center text-center text-2xl
-            text-pretty text-theme-10
-          `}
-        >
-          {text}
-        </span>
-      </div>
-    )
+    input.click()
   }
-)
+
+  const handleFiles = async (fileList: FileList) => {
+    const readFile = (file: File): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = () => resolve(reader.result as string)
+        reader.onerror = () => reject(reader.error)
+        reader.readAsText(file)
+      })
+    }
+
+    const files = Array.from(fileList)
+    if (!files.length) return
+
+    const contents = await Promise.all(files.map(file => readFile(file)))
+    onDropOrSelect(contents)
+  }
+
+  const [twBaseStyles, twIconStyles, text] = isDragginOver
+    ? ['border-theme-10 animate-pulse', 'animate-bounce', 'Drop your files here!']
+    : ['', '', children]
+
+  return (
+    <div
+      className={twMerge(`
+        relative border-4 border-theme-10/20 border-dashed rounded-xl
+        flex flex-col gap-2 items-center justify-center p-8
+        hover:border-theme-10/40 active:scale-99 active:brightness-90 transition
+        ${twBaseStyles} ${className}
+      `)}
+      onClick={handleClick}
+      {...props}
+    >
+      <ColoredPixelatedImage icon={icon} className={`size-16 ${twIconStyles}`} />
+      <span
+        className={`
+          h-16 flex items-center justify-center text-center text-2xl
+          text-pretty text-theme-10
+        `}
+      >
+        {text}
+      </span>
+    </div>
+  )
+}
